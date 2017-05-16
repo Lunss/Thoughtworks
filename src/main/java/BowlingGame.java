3de899ba5ebe0174b37a4a355f784f4794f373a2
2625349336@qq.com <2625349336@qq.com>
2017-05-16 21:41:13 +0800

    in class and add package in bowlinggame.java

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,6 +1,53 @@
+package main.java;
 public class BowlingGame {
-
     public int getBowlingScore(String bowlingCode) {
+        class Ge{
+            int flag; //flag=1表示strike，为2表示spare.
+            int first;
+            int second;
+
+            public Ge(String s,int last) {
+                if (s.length() == 1) {
+                   if(last == 0) {
+                       flag = 1;
+                       first = 10;
+                       second = 0;
+                   }else{
+                       flag = 0;second=0;
+                       char a = s.charAt(0);
+                       if(a=='X'){
+                           first = 10;
+                       }else if(a=='-'){
+                           first = 0;
+                       }else{
+                           first = a-'0';
+                       }
+                   }
+                } else if (s.length() == 2) {
+                    flag = 0;
+                    char a1 = s.charAt(0);
+                    char a2 = s.charAt(1);
+                    if (a1 == '-') {
+                        first = 0;
+                    } else if (a1 == 'X') {
+                        first = 10;
+                    } else {
+                        first = a1 - '0';
+                    }
+                    if (a2 == '-') {
+                        second = 0;
+                    } else if (a2 == 'X') {
+                        second = 10;
+                    } else if (a2 == '/') {
+                        flag = 2;
+                        flag = 2;
+                        second = 10 - first;
+                    } else {
+                        second = a2 - '0';
+                    }
+                }
+            }
+        }
        	String[] ss = bowlingCode.split("\\|");
         Ge[] ges = new Ge[11];
         int sum = 0;
@@ -32,53 +79,5 @@ public class BowlingGame {
         }
         System.out.println(sum);
         return 0;
-    }
-}
-
-class Ge{
-    int flag; //flag=1表示strike，为2表示spare.
-    int first;
-    int second;
-
-    public Ge(String s,int last) {
-        if (s.length() == 1) {
-           if(last == 0) {
-               flag = 1;
-               first = 10;
-               second = 0;
-           }else{
-               flag = 0;second=0;
-               char a = s.charAt(0);
-               if(a=='X'){
-                   first = 10;
-               }else if(a=='-'){
-                   first = 0;
-               }else{
-                   first = a-'0';
-               }
-           }
-        } else if (s.length() == 2) {
-            flag = 0;
-            char a1 = s.charAt(0);
-            char a2 = s.charAt(1);
-            if (a1 == '-') {
-                first = 0;
-            } else if (a1 == 'X') {
-                first = 10;
-            } else {
-                first = a1 - '0';
-            }
-            if (a2 == '-') {
-                second = 0;
-            } else if (a2 == 'X') {
-                second = 10;
-            } else if (a2 == '/') {
-                flag = 2;
-                flag = 2;
-                second = 10 - first;
-            } else {
-                second = a2 - '0';
-            }
-        }
     }
 }
